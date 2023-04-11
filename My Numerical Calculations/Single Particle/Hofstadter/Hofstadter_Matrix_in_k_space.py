5bd9b9a806a95af009b87572e2d638845aed10b1
Umut_Can_Turhan <umutcanturhan3@gmail.com>
2023-04-11 20:14:54 +0300

    .

--- My Numerical Calculations/Single Particle/Hofstadter/Hofstadter_Matrix_in_k_space.py
+++ My Numerical Calculations/Single Particle/Hofstadter/Hofstadter_Matrix_in_k_space.py
@@ -1,13 +1,14 @@
 import numpy as np
-
-# Initial value of q
-q=5
-
-#Hamiltonian Matrix for Hofstadter Model (doesn't include alpha=1/2 matrix)
-def HMatrix(alfa, k_x, k_y):
-    M = np.zeros((q,q), dtype=complex) 
-    for i in range (0, q):
-        M[i,i]=2*np.cos(k_y-2*np.pi*alfa*i) 
+# Define Harper-Hofstadter Matrix
+def H(p,q,kx,ky):
+    # Define magnetic flux per unit-cell
+    alpha = p/q
+    # qxq size of zero matrix
+    M = np.zeros((q,q), dtype=complex)
+    for i in range(0,q):
+        # Ortogonal elements of matris
+        M[i,i] = 2*np.cos(ky-2*np.pi*alpha*i)
+        # Other elements
         if i==q-1: 
             M[i,i-1]=1
         elif i==0: 
@@ -15,24 +16,29 @@ def HMatrix(alfa, k_x, k_y):
         else: 
             M[i,i-1]=1
             M[i,i+1]=1
-        M[0,q-1]=np.exp(-q*1.j*k_x)
-        M[q-1,0]=np.exp(q*1.j*k_x)
+    # Bloch condition
+    if q==2:
+        M[0,q-1] = 1+np.exp(-q*1.j*kx)
+        M[q-1,0] = 1+np.exp(q*1.j*kx)
+    else:
+        M[0,q-1] = np.exp(-q*1.j*kx)
+        M[q-1,0] = np.exp(q*1.j*kx)
     return M
 
-#Special Hofstadter Matrix Just alpha=1/2
-s = 2
-alpha = 1/s
-def HMatrix2(alpha, k_x, k_y):
-    M2 = np.zeros((s,s), dtype=complex) 
-    for i in range (0, s):
-        M2[i,i]=2*np.cos(k_y-2*np.pi*alpha*i) 
-        if i==q-1: 
-            M2[i,i-1]=1
-        M2[0,s-1]=1+np.exp(-s*1.j*k_x)
-        M2[s-1,0]=1+np.exp(s*1.j*k_x)
-    return M2
+# #Special Hofstadter Matrix Just alpha=1/2
+# s = 2
+# alpha = 1/s
+# def HMatrix2(alpha, k_x, k_y):
+#     M2 = np.zeros((s,s), dtype=complex) 
+#     for i in range (0, s):
+#         M2[i,i]=2*np.cos(k_y-2*np.pi*alpha*i) 
+#         if i==q-1: 
+#             M2[i,i-1]=1
+#         M2[0,s-1]=1+np.exp(-s*1.j*k_x)
+#         M2[s-1,0]=1+np.exp(s*1.j*k_x)
+#     return M2
 
-#Set Rational to Alpha Values 
-def gcd(a, b): 
-    if b == 0: return a
-    return gcd(b, a % b)
+# #Set Rational to Alpha Values 
+# def gcd(a, b): 
+#     if b == 0: return a
+#     return gcd(b, a % b)
